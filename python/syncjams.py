cc770ff7904b8d83aeb3f0db0a702d44e840827b
Chris McCormick <chris@mccormick.cx>
2014-10-08 21:53:04 +0800

    Callbacks for node joining and leaving. All nodes now look out for zombie node states and keep them going for new nodes joining so that states are never lost when nodes disconnect.

--- python/syncjams.py
+++ python/syncjams.py
@@ -114,6 +114,12 @@ class SyncjamsNode:
     def state(self, node_id, address, *args):
         pass
     
+    def node_joined(self, node_id):
+        pass
+    
+    def node_left(self, node_id):
+        pass
+    
     ### Private methods. ###
     
     def _process_tick(self):
@@ -133,18 +139,32 @@ class SyncjamsNode:
     
     def _forget_old_nodes(self, now):
             # find nodes we have not hear from for more than timeout
-            forget = [n for n in self.last_seen if (self.last_seen[n] + NODE_TIMEOUT < now)]
+            forget = [node_id for node_id in self.last_seen if (self.last_seen[node_id] + NODE_TIMEOUT < now)]
             if forget:
                 logging.info("forgetting nodes %s" % forget)
             # remove references to those nodes
-            for n in forget:
-                del self.last_seen[n]
-                if self.last_messages.has_key(n):
-                    del self.last_messages[n]
+            for node_id in forget:
+                # actually remove this node from all of our lists
+                del self.last_seen[node_id]
+                if self.last_messages.has_key(node_id):
+                    del self.last_messages[node_id]
+                # make sure the node is not listed as initiator of any states
+                for s in self.states:
+                    if self.states[s][0] == node_id:
+                        self.states[s][0] == -1
+                # run the node_left callback method
+                self.node_left(node_id)
+    
+    def _get_orphaned_states(self):
+        return [s for s in self.states if not self.states[s][0] in self.last_seen.keys()]
     
     def _broadcast_tick(self):
         # broadcast what we think the current tick is to the network - and also our last known message IDs from peers
-        self._send_one_to_all("/tick", [self.node_id, self.last_tick[0]] + sum([[m, self.last_messages[m]] for m in self.last_messages], []))
+        self._send_one_to_all("/tick",
+            [self.node_id, self.last_tick[0]] +
+            sum([[m, self.last_messages[m]] for m in self.last_messages], []) +
+            sum([list(self.states[s][:2]) for s in self._get_orphaned_states()], [])
+        )
     
     def _send(self, address, message=[]):
         if not address.startswith("/"):
@@ -188,9 +208,6 @@ class SyncjamsNode:
             self._drop("No node_id", addr, tags, packet_copy, source)
             return
         
-        # record when we last saw this particular node
-        self.last_seen[node_id] = time.time()
-        
         # digestible format for our address routing
         route = addr[len(self.namespace) + 1:].split("/")
         
@@ -206,29 +223,33 @@ class SyncjamsNode:
                 self.tick(*self.last_tick)
                 # send out our new tick anyway so everyone learns our last_message list
                 self._broadcast_tick()
-            # check if we found our id and pull out their last message id from us too
-            found = [[packet[x*2], packet[x*2+1]] for x in range(len(packet) / 2) if packet[x*2] == self.node_id]
-            for f in found:
-                    # send through the messages that they are missing message_id, address, message
-                    [self._send_one_to_all(m[1], m[2]) for m in self.sent_queue if m[0] > f[1]]
-            if not len(found):
-                    # send through our last message plus the entire state
-                    if self.sent_queue:
-                        self._send_one_to_all(*(self.sent_queue[-1][1:]))
-                    # state flood to update them
-                    [self._send_one_to_all("/state" + s, self.states[s]) for s in self.states]
+            # build a dictionary of their known nodes and latest message ids
+            their_latest_messages = dict([(packet[x*2], packet[x*2+1]) for x in range(len(packet) / 2)])
+            # send through the messages from me that they are missing message_id, address, message
+            [self._send_one_to_all(m[1], m[2]) for m in self.sent_queue if m[0] > their_latest_messages.get(self.node_id, 0)]
+            # find zombie states that have an id that is not in their list and is a dead node (can't rebroadcast itself)
+            for s in self.states:
+                if not their_latest_messages.has_key(self.states[s][0]):
+                    # rebroadcast the state message
+                    self._send_one_to_all("/bash-state" + s, self.states[s])
+            # if this is the first time we have seen this node then run the callback method
+            if not self.last_seen.has_key(node_id):
+                self.node_joined(node_id)
+            # update the last seen time
+            self.last_seen[node_id] = time.time()
         # state or message packet
         else:
             # every message should contain a message id
             message_id = self._parse_number_slot(packet)
             # if this is the next in the sequence from this client then increment that counter
-            if not ((self.last_messages.get(node_id, None) is None) or (message_id == self.last_messages[node_id] + 1)):
+            if not ((self.last_messages.get(node_id, None) is None) or (message_id == self.last_messages[node_id] + 1) or route[0] == "bash-state"):
                 self._drop("Old message", addr, tags, packet_copy, source)
             else:
                 # log that we have received latest message from this client
-                self.last_messages[node_id] = message_id
+                if message_id > self.last_messages.get(node_id, 0):
+                    self.last_messages[node_id] = message_id
                 # received state update message
-                if len(route) and route[0] == "state":
+                if len(route) and route[0] == "state" or route[0] == "bash-state":
                     tick = self._parse_number_slot(packet)
                     timediff = self._parse_number_slot(packet, coorce=float)
                     key = "/" + "/".join(route[1:])
@@ -248,6 +269,7 @@ class SyncjamsNode:
         oscmsg.setAddress(self.namespace + address)
         # add the message parts to it
         [oscmsg.append(m) for m in message]
+        logging.info("raw sent packet %s" % (oscmsg,))
         # send one message out to all broadcast/multicast networks possible, ignoring errors
         for a in ADDRESSES:
             try:
@@ -343,6 +365,12 @@ if __name__ == "__main__":
         
         def message(self, node_id, address, *message):
             print "Message to %s = %s (from node %d)" % (address, message, node_id)
+        
+        def node_joined(self, node_id):
+            print "Node %d joined." % node_id
+        
+        def node_left(self, node_id):
+            print "node %d left." % node_id
     
     # Start a test syncjams instance
     s = TestSyncjamsNode(loglevel=len(sys.argv) > 1 and getattr(logging, sys.argv[1]) or logging.ERROR)
